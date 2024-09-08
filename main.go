7abe123e2da38d5357f3d17ac1aff9bd9854ff3e
yojaouhar <jwhryosf@gmail.com>
2024-09-08 18:58:01 +0100

    2

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"goreloaded/project"
+	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	"goreloaded/functions"
 )
 
 func main() {
@@ -11,10 +14,60 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	data2 := project.CleanStr(string(data))
-
-	err = os.WriteFile("result.txt", []byte(data2), 0o666)
+	data2 := Change(string(data))
+	result := ""
+	for _, char := range data2 {
+		result += string(char)
+	}
+	err = os.WriteFile("result.txt", []byte(result), 0o666)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Change(data string) []string {
+	var result []string
+	res := ""
+
+	for _, char := range data {
+		if char == ')' {
+			if res != "" {
+				res += string(char)
+
+				if len(strings.Split(res, " ")) != 1{
+					result = append(result, edit(res))
+				}
+				
+				res = ""
+
+			}
+		} else {
+			res += string(char)
+		}
+	}
+	if res != "" {
+		result = append(result, edit(res))
+	}
+	fmt.Println([]byte(strings.Join(result, "")))
+	return result 
+}
+
+func edit(res string) string {
+	data3 := strings.Split(res, " ")
+	data4 := ""
+	for i, char := range data3 {
+		if char == "(cap)" {
+			data4 += functions.Capitalize(data3[i-1])
+			data4 = strings.Replace(data4, data3[i-1], "", -1)
+		} else if char == "(low)" {
+			data4 += strings.ToLower(data3[i-1])
+			data4 = strings.Replace(data4, data3[i-1], "", -1)
+		} else if char == "(up)" {
+			data4 += strings.ToUpper(data3[i-1])
+			data4 = strings.Replace(data4, data3[i-1], "", -1)
+		} else {
+			data4 += char + " "
+		}
+	}
+	return data4
 }
--- project/project.go
+++
@@ -1,9 +0,0 @@
-package project
-
-import (
-	"strings"
-)
-
-func CleanStr(data string) string {
-	return strings.Join(strings.Fields(data), " ")
-}
