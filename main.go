d5fd39cd525603c2a1e99e10f9d686796333791b
yojaouhar <jwhryosf@gmail.com>
2024-09-09 18:22:18 +0100

    2

--- main.go
+++ main.go
@@ -1,73 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
-	"strings"
-
-	"goreloaded/functions"
 )
 
 func main() {
 	data, err := os.ReadFile("sample.txt")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	data2 := Change(string(data))
-	result := ""
-	for _, char := range data2 {
-		result += string(char)
-	}
-	err = os.WriteFile("result.txt", []byte(result), 0o666)
+	data2 := div(string(data))
+
+	err = os.WriteFile("result.txt", []byte(data2), 0o666)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
-func Change(data string) []string {
-	var result []string
+func div(data string) string {
 	res := ""
-
+	var result []string
 	for _, char := range data {
 		if char == ')' {
-			if res != "" {
-				res += string(char)
+			res += string(char)
+			result = append(result, tretement(res)+"\n")
 
-				if len(strings.Split(res, " ")) != 1{
-					result = append(result, edit(res))
-				}
-				
-				res = ""
-
-			}
+		} else if char == '\n' {
+			res = ""
 		} else {
 			res += string(char)
 		}
 	}
 	if res != "" {
-		result = append(result, edit(res))
+		result = append(result, tretement(res)+"\n")
 	}
-	fmt.Println([]byte(strings.Join(result, "")))
-	return result 
+	resultt := ""
+	for _, char := range result {
+		resultt += char
+	}
+	return resultt
 }
 
-func edit(res string) string {
-	data3 := strings.Split(res, " ")
-	data4 := ""
-	for i, char := range data3 {
-		if char == "(cap)" {
-			data4 += functions.Capitalize(data3[i-1])
-			data4 = strings.Replace(data4, data3[i-1], "", -1)
-		} else if char == "(low)" {
-			data4 += strings.ToLower(data3[i-1])
-			data4 = strings.Replace(data4, data3[i-1], "", -1)
-		} else if char == "(up)" {
-			data4 += strings.ToUpper(data3[i-1])
-			data4 = strings.Replace(data4, data3[i-1], "", -1)
-		} else {
-			data4 += char + " "
-		}
-	}
-	return data4
+func tretement(balance string) string {
+	return balance
 }
